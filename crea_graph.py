ed1138759ac145629f029d90541b18b8f351a981
uvsq22101464 <91185466+uvsq22101464@users.noreply.github.com>
2023-05-05 00:05:35 +0200

    proto

--- crea_graph.py
+++ crea_graph.py
@@ -30,6 +30,7 @@ x2, y2 = None, None
 text = ""
 taille = 0
 dico = {}
+end_node, start_node = None, None
 
 def motion(event):
     global status, x1, y1, x2, y2, taille
@@ -46,14 +47,14 @@ def zero(event=None):
     x1, y1 = None, None
     x2, y2 = None, None
     dico[text] = taille
-    canvas.unbind('<Button-1>')
+    canvas.unbind('<Button-3>')
     root.unbind('<Escape>')
+    return taille
 
-def recup_text():
+def recup_text(event):
     global text
     text = entry.get()
-    entry.delete(0, END)
-    canvas.bind('<Button-1>', motion)
+    canvas.bind('<Button-3>', motion)
     root.bind('<Escape>', zero)
 
 def draw_nodes(event):
@@ -68,13 +69,62 @@ def load_data():
     with open("nodes.txt", "r") as fic:
         for lines in fic.readlines():
             canvas.create_oval(float(lines.split("\t")[1])*adjust_x - 5, float(lines.split("\t")[2])*adjust_y - 5, float(lines.split("\t")[1])*adjust_x + 5, float(lines.split("\t")[2])*adjust_y + 5, fill="black")
-            
+
+def find(event):
+    if canvas.find_closest(event.x, event.y)[0] != 1:
+        return canvas.find_closest(event.x, event.y)[0]
+    else:
+        raise ValueError("Il faut cliquer sur les cercles sinon c'est faux")
+    
+def save_arc(event):
+    global end_node, start_node, color, taille
+    print("go", start_node, end_node)
+    with open("nodes.txt", "r+") as fic:
+        lines = fic.readlines()
+        fic.seek(0)
+        fic.truncate()
+        for line in lines:
+            split = line.split("\t")
+            if int(split[0]) == start_node:
+                piste = eval(split[-1])
+                piste.append((end_node, text, color, taille))    
+                content = split[0] + "\t" + split[1] + "\t" + split[2] + "\t" + str(piste) + "\n"
+                fic.write(content)
+            else:
+                fic.write(line)
+
+
+def draw_arc():
+    global text, color
+    text = entry.get()
+    color = entry2.get()
+    #entry.delete(0, END)
+    canvas.focus_force()
+    canvas.bind('<Button-1>', save_arc)
+    canvas.bind('a', recup_start_node)
+    canvas.bind('z', recup_end_node)
+    canvas.bind('<Button-3>', motion)
+    root.bind('<Escape>', zero)
+
+def recup_start_node(event):
+    global start_node
+    start_node = find(event)
+    print(start_node)
+    return start_node
+
+def recup_end_node(event):
+    global end_node
+    end_node = find(event)
+    print(end_node)
+    return end_node
 
 label = Label(root, text='Courchevel', height=3, font=('Calibri', 23))
 label.pack()
 entry = Entry(root)
 entry.pack()
-button1 = Button(root, text='Valider', command=recup_text)
+entry2 = Entry(root)
+entry2.pack()
+button1 = Button(root, text='Valider', command=draw_arc)
 button1.pack()
 label_vide = Label(root)
 label_vide.pack()
@@ -85,7 +135,7 @@ button3.pack()
 button4 = Button(root, text="CHARGER SAVE", command=load_data)
 button4.pack()
 
-canvas.bind('<Button-2>', lambda ev : print(ev.x, ev.y))
+#canvas.bind('<Button-2>', find)
 
 root.mainloop()
 print(dico)
